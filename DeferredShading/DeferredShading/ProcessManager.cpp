45717bbe913b3c8388f055647afa1ea0c0800bce
양현찬 <e7315126@naver.com>
2014-10-15 17:42:03 +0900

    프로세스 메니져 파일 추가

---
+++ DeferredShading/DeferredShading/ProcessManager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "ProcessManager.h"
+
+
+ProcessManager::ProcessManager()
+{
+}
+
+
+ProcessManager::~ProcessManager()
+{
+}
---
+++ DeferredShading/DeferredShading/ProcessManager.h
@@ -0,0 +1,10 @@
+#pragma once
+class ProcessManager
+{
+public:
+	ProcessManager();
+	~ProcessManager();
+
+
+};
+
