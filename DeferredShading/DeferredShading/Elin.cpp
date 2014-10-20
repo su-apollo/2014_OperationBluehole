96baf39d0aa418a7dfde4fb1c2108d5654537e9e
junggang <missjingi@gmail.com>
2014-10-21 01:45:53 +0900

    Elin 버텍스, 인덱스 받아오는 부분 수정

--- DeferredShading/DeferredShading/Elin.cpp
+++ DeferredShading/DeferredShading/Elin.cpp
@@ -1,9 +1,9 @@
 #include "stdafx.h"
 #include "Elin.h"
-#include "MacroSet.h"
-
-
-
+#include "Renderer.h"
+#include "LightManager.h"
+#include "Timer.h"
+#include "Logger.h"
 
 
 Elin::Elin()
@@ -15,7 +15,7 @@ Elin::~Elin()
 {
 }
 
-BOOL Elin::Initialize()
+BOOL Elin::Init()
 {
 	mFbxManager = FbxManager::Create();
 	if (!mFbxManager)
@@ -27,6 +27,28 @@ BOOL Elin::Initialize()
 	mFbxManager->SetIOSettings(fbxIOSettings);
 
 	mFbxScene = FbxScene::Create(mFbxManager, "myScene");
+
+	if (!LoadFBX())
+	{
+		return false;
+	}
+
+	//
+	mD3DDevice = Renderer::GetInstance()->GetDevice();
+	mD3DDeviceContext = Renderer::GetInstance()->GetDeviceContext();
+	D3DXMatrixIdentity(&mWorld);
+	mPolygonCount = 0;
+
+
+	if (!CompileShader())
+		return FALSE;
+
+	if (!CreateBuffer())
+		return FALSE;
+
+
+	LightManager::GetInstance()->CreateDirectionalLight(MAX_LIGHT);
+	//
 
 	return TRUE;
 
@@ -42,6 +64,7 @@ BOOL Elin::LoadFBX()
 	}
 
 	const char* filePath = "C:\\ElinModel\\Popori_F_H00_dance.FBX";
+	//const char* filePath = "C:\\kim\\MI.FBX";
 
 	if (!fbxImporter->Initialize(filePath, -1, mFbxManager->GetIOSettings()))
 	{
@@ -58,14 +81,6 @@ BOOL Elin::LoadFBX()
 
 	ProcessGeometry(mFbxScene->GetRootNode());
 
-
-
-
-
-
-
-
-
 	return TRUE;
 }
 
@@ -73,90 +88,119 @@ BOOL Elin::LoadFBX()
 
 void Elin::ProcessGeometry(FbxNode* inNode)
 {
-	//get data only from mesh (FbxNodeAttribute::eMesh)
-	if (inNode->GetNodeAttribute())
-	{
-		switch (inNode->GetNodeAttribute()->GetAttributeType())
-		{
-		case FbxNodeAttribute::eMesh:
-			//1.get control points
-			ProcessControlPoints(inNode);
-			//2.traverse vertices from triangles
-			//ProcessMesh(inNode);
+	FbxMesh *pNewMesh = nullptr;
 
-			break;
+	if (inNode)
+	{
+		int ChildCount = inNode->GetChildCount();
+
+		for (int i = 0; i < ChildCount; i++)
+		{
+			FbxNode* childNode = inNode->GetChild(i);
+
+			if (childNode->GetNodeAttribute() == NULL)
+				continue;
+
+			if (childNode->GetNodeAttribute()->GetAttributeType() != FbxNodeAttribute::eMesh)
+				continue;
+
+			FbxMesh* pMesh = (FbxMesh*)childNode->GetNodeAttribute();
+
+			FbxLayerElementArrayTemplate<FbxVector4>* normal = 0;
+			pMesh->GetNormals(&normal);
+			FbxLayerElementArrayTemplate<FbxVector2>* uv = 0;
+			pMesh->GetTextureUV(&uv, FbxLayerElement::eTextureDiffuse);
+
+			FbxVector4* pVertices = pMesh->GetControlPoints();
+			const int lVertexCount = pMesh->GetControlPointsCount();
+
+			//fbx매쉬 생성
+			if (pMesh == NULL)
+			{
+				Log("Mesh is null\n");
+			}
+			else
+			{
+				// ========= Get the Vertices ==============================
+				int numVerts = pMesh->GetControlPointsCount();
+				Vertex tempVerts;
+
+				for (int j = 0; j < numVerts; j++)
+				{
+					D3DXVECTOR3 currPosition;
+					currPosition.x = static_cast<float>(pMesh->GetControlPointAt(i).mData[0]);
+					currPosition.y = static_cast<float>(pMesh->GetControlPointAt(i).mData[1]);
+					currPosition.z = static_cast<float>(pMesh->GetControlPointAt(i).mData[2]);
+
+					tempVerts.mPos = currPosition;
+
+					float tU;
+					float tV;
+
+					tU = (float)(*uv)[j].mData[0];
+					tV = (float)(*uv)[j].mData[1];
+					tempVerts.mTU = tU;
+					tempVerts.mTV = tV;
+
+					mVertices.push_back(tempVerts);
+				}
+
+				if (numVerts == 0)
+					return;
+
+				for (int j = 0; j < pMesh->GetPolygonCount(); j++)
+				{
+					int iNumVertices = pMesh->GetPolygonSize(j);
+
+					if (iNumVertices != 3)
+						return;
+
+					Indices tempIndex;
+
+					for (int k = 0; k < iNumVertices; k++)
+					{
+						int iControlPointIndex = pMesh->GetPolygonVertex(j, k);
+
+						FbxVector4 normal;
+						pMesh->GetPolygonVertexNormal(j, k, normal);
+
+						// ========= Get the Normals ==============================
+						D3DXVECTOR3 currNormal;
+						//이게 좀 많이 헷갈림
+						// 					tempVerts[iControlPointIndex].mNormal.x = (float)normal.mData[0];
+						// 					tempVerts[iControlPointIndex].mNormal.y = (float)normal.mData[1];
+						// 					tempVerts[iControlPointIndex].mNormal.z = (float)normal.mData[2];
+						mVertices[iControlPointIndex].mNormal.x = (float)normal.mData[0];
+						mVertices[iControlPointIndex].mNormal.y = (float)normal.mData[1];
+						mVertices[iControlPointIndex].mNormal.z = (float)normal.mData[2];
+
+						// ========= Get the Indices ==============================
+						switch (k)
+						{
+						case 0:
+							tempIndex.i0 = iControlPointIndex;
+							break;
+						case 1:
+							tempIndex.i1 = iControlPointIndex;
+							break;
+						case 2:
+							tempIndex.i2 = iControlPointIndex;
+							break;
+						default:
+							break;
+						}
+					}
+					//mIndex.push_back(tempIndex);
+					mIndices.push_back(tempIndex.i0);
+					mIndices.push_back(tempIndex.i1);
+					mIndices.push_back(tempIndex.i2);
+
+				}
+			} // else
+			ProcessGeometry(childNode);
 		}
 	}
-
-	for (int i = 0; i < inNode->GetChildCount(); ++i)
-	{
-		ProcessGeometry(inNode->GetChild(i));
-	}
-
-
 }
-
-
-void Elin::ProcessControlPoints(FbxNode* inNode)
-{
-
-	FbxMesh* currMesh = inNode->GetMesh();
-	unsigned int ctrlPointCount = currMesh->GetControlPointsCount();
-	for (unsigned int i = 0; i < ctrlPointCount; ++i)
-	{
-		ControlPoint* currCtrlPoint = new ControlPoint();
-		D3DXVECTOR3 currPosition;
-		currPosition.x = static_cast<float>(currMesh->GetControlPointAt(i).mData[0]);
-		currPosition.y = static_cast<float>(currMesh->GetControlPointAt(i).mData[1]);
-		currPosition.z = static_cast<float>(currMesh->GetControlPointAt(i).mData[2]);
-		currCtrlPoint->mPos = currPosition;
-		mControlPoints[i] = currCtrlPoint;
-	}
-
-}
-
-
-void Elin::ProcessMesh(FbxNode* inNode)
-{
-	FbxMesh* currMesh = inNode->GetMesh();
-
-	mTriangleCount = currMesh->GetPolygonCount();
-	int vertexCounter = 0;
-	mTriangles.reserve(mTriangleCount);
-
-	for (unsigned int i = 0; i < mTriangleCount; ++i)
-	{
-		Triangle currTriangle;
-		mTriangles.push_back(currTriangle);
-
-		for (unsigned int j = 0; j < 3; ++j)
-		{
-			int ctrlPointIndex = currMesh->GetPolygonVertex(i, j);
-			ControlPoint* currCtrlPoint = mControlPoints[ctrlPointIndex];
-			Vertex temp;
-			temp.mPos = currCtrlPoint->mPos;
-
-			mVertices.push_back(temp);
-			mTriangles.back().mIndices.push_back(vertexCounter);
-			++vertexCounter;
-		}
-	}
-
-
-
-
-
-
-	//we don't need controlpoints anymore!
-	for (auto itr = mControlPoints.begin(); itr != mControlPoints.end(); ++itr)
-	{
-		delete itr->second;
-	}
-	mControlPoints.clear();
-}
-
-
-
 
 std::string Elin::GetFileName(const char* filePath)
 {
@@ -182,9 +226,200 @@ std::string Elin::GetFileName(const char* filePath)
 
 void Elin::CleanUp()
 {
-
-
 	mFbxScene->Destroy();
 	mFbxManager->Destroy();
 
+	mIndices.clear();
+
+	mVertices.clear();
+
 }
+
+BOOL Elin::CompileShader()
+{
+	if (!CompileVertexShader())
+		return FALSE;
+
+	if (!CompilePixelShader())
+		return FALSE;
+
+	return TRUE;
+}
+
+BOOL Elin::CreateBuffer()
+{
+	if (!CreateVertexBuff())
+		return FALSE;
+	if (!CreateIndexBuff())
+		return FALSE;
+	if (!CreateConstBuff())
+		return FALSE;
+	return TRUE;
+}
+
+void Elin::Render()
+{
+	//rotate
+	D3DXMATRIX matRotate;
+	D3DXMatrixRotationY(&matRotate, Timer::GetInstance()->GetDeltaTime());
+	mWorld *= matRotate;
+
+	//update constbuff
+	D3DXMATRIX matWorld;
+	D3DXMATRIX matView = Camera::GetInstance()->GetMatView();
+	D3DXMATRIX matProj = Camera::GetInstance()->GetMatProj();
+	EVSConstantBuffer vcb;
+
+	//열 우선배치
+	D3DXMatrixTranspose(&matWorld, &mWorld);
+	D3DXMatrixTranspose(&matView, &matView);
+	D3DXMatrixTranspose(&matProj, &matProj);
+	vcb.mWorld = matWorld;
+	vcb.mView = matView;
+	vcb.mProjection = matProj;
+	mD3DDeviceContext->UpdateSubresource(mVSConstBuffer, 0, NULL, &vcb, 0, 0);
+
+	EPSConstantBuffer pcb;
+	DLightPointer light1 = LightManager::GetInstance()->mDLightList[0];
+	DLightPointer light2 = LightManager::GetInstance()->mDLightList[1];
+	pcb.vLightDir[0] = light1->GetDirection();
+	pcb.vLightDir[1] = light2->GetDirection();
+	pcb.vLightColor[0] = light1->GetColor();
+	pcb.vLightColor[1] = light2->GetColor();
+	mD3DDeviceContext->UpdateSubresource(mPSConstBuffer, 0, NULL, &pcb, 0, 0);
+
+	//draw
+	mD3DDeviceContext->VSSetShader(mVertexShader, NULL, 0);
+	mD3DDeviceContext->VSSetConstantBuffers(0, 1, &mVSConstBuffer);
+	mD3DDeviceContext->PSSetShader(mPixelShader, NULL, 0);
+	mD3DDeviceContext->PSSetConstantBuffers(0, 1, &mPSConstBuffer);
+	mD3DDeviceContext->DrawIndexed(mIndices.size(), 0, 0);
+
+}
+
+HRESULT Elin::CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut)
+{
+	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+
+#if defined( DEBUG ) || defined( _DEBUG )
+	dwShaderFlags |= D3DCOMPILE_DEBUG;
+#endif
+
+	ID3DBlob* pErrorBlob;
+	hr = D3DX11CompileFromFile(szFileName, NULL, NULL, szEntryPoint, szShaderModel,
+		dwShaderFlags, 0, NULL, ppBlobOut, &pErrorBlob, NULL);
+	if (FAILED(hr))
+	{
+		if (pErrorBlob != NULL)
+			OutputDebugStringA((char*)pErrorBlob->GetBufferPointer());
+		if (pErrorBlob) pErrorBlob->Release();
+		return hr;
+	}
+	if (pErrorBlob) pErrorBlob->Release();
+
+	return S_OK;
+}
+
+BOOL Elin::CompileVertexShader()
+{
+	//
+	return TRUE;
+}
+
+BOOL Elin::CompilePixelShader()
+{
+	//
+	return TRUE;
+}
+
+BOOL Elin::CreateVertexBuff()
+{
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+	D3D11_SUBRESOURCE_DATA InitData;
+	ZeroMemory(&InitData, sizeof(InitData));
+
+	// Create vertex buffer
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	//문제
+	int numVertices = mVertices.size();
+	bd.ByteWidth = sizeof(Vertex) * numVertices;
+
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	InitData.pSysMem = &mVertices;
+	hr = Renderer::GetInstance()->GetDevice()->CreateBuffer(&bd, &InitData, &mVertexBuffer);
+	if (FAILED(hr))
+		return FALSE;
+
+	// Set vertex buffer
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	Renderer::GetInstance()->GetDeviceContext()->IASetVertexBuffers(0, 1, &mVertexBuffer, &stride, &offset);
+
+	return TRUE;
+}
+
+BOOL Elin::CreateIndexBuff()
+{
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+	D3D11_SUBRESOURCE_DATA InitData;
+	ZeroMemory(&InitData, sizeof(InitData));
+
+	// Create index buffer
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	int numTriangle = mIndices.size();
+	bd.ByteWidth = sizeof(int) * numTriangle;        // 36 vertices needed for 12 triangles in a triangle list
+	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	InitData.pSysMem = &mIndices;
+	hr = Renderer::GetInstance()->GetDevice()->CreateBuffer(&bd, &InitData, &mIndexBuffer);
+	if (FAILED(hr))
+		return FALSE;
+
+	// Set index buffer
+	Renderer::GetInstance()->GetDeviceContext()->IASetIndexBuffer(mIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+
+	// Set primitive topology
+	Renderer::GetInstance()->GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	return TRUE;
+}
+
+BOOL Elin::CreateConstBuff()
+{
+	D3D11_BUFFER_DESC bd;
+	//아래 zeromemory를 꼭해야함
+	ZeroMemory(&bd, sizeof(bd));
+	// Create the constant buffer
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(VSConstantBuffer);
+	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	bd.CPUAccessFlags = 0;
+
+	hr = mD3DDevice->CreateBuffer(&bd, NULL, &mVSConstBuffer);
+	if (FAILED(hr))
+		return FALSE;
+
+	bd.ByteWidth = sizeof(PSConstantBuffer);
+	hr = mD3DDevice->CreateBuffer(&bd, NULL, &mPSConstBuffer);
+	if (FAILED(hr))
+		return FALSE;
+
+
+	return TRUE;
+}
+
+void Elin::Release()
+{
+	// 	Saferelease(mVSConstBuffer);
+	// 	Saferelease(mPSConstBuffer);
+	// 	Saferelease(mVertexBuffer);
+	// 	Saferelease(mIndexBuffer);
+	// 
+	// 	Saferelease(mVertexLayout11);
+	// 	Saferelease(mVertexShader);
+	// 	Saferelease(mPixelShader);
+}
+
--- DeferredShading/DeferredShading/Elin.h
+++ DeferredShading/DeferredShading/Elin.h
@@ -1,26 +1,37 @@
 #pragma once
+
 #include <fbxsdk.h>
 #include <xnamath.h>
-#include <unordered_map>
 
 struct Vertex
 {
 	D3DXVECTOR3 mPos;
-	//D3DXVECTOR3 mNormal;
-	//uv
+	D3DXVECTOR3 mNormal;
+	float mTU;
+	float mTV;
+
 };
 
-struct ControlPoint
+struct Indices
 {
-	D3DXVECTOR3 mPos;
+	int i0, i1, i2;
 };
 
-struct Triangle
+
+static const int EMAX_LIGHT = 2;
+
+struct EVSConstantBuffer
 {
-	std::vector<unsigned int> mIndices;
+	D3DXMATRIX mWorld;
+	D3DXMATRIX mView;
+	D3DXMATRIX mProjection;
 };
 
-
+struct EPSConstantBuffer
+{
+	D3DXVECTOR4 vLightDir[2];
+	D3DXVECTOR4 vLightColor[2];
+};
 
 
 class Elin
@@ -29,9 +40,15 @@ public:
 	Elin();
 	~Elin();
 
-	BOOL Initialize();
+	BOOL Init();
 	BOOL LoadFBX();
 	void CleanUp();
+
+	BOOL CompileShader();
+	BOOL CreateBuffer();
+
+	void Render();
+	void Release();
 
 
 private:
@@ -40,20 +57,40 @@ private:
 	FbxScene* mFbxScene = nullptr;
 
 	void ProcessGeometry(FbxNode* inNode);
-	void ProcessControlPoints(FbxNode* inNode);
-	void ProcessMesh(FbxNode* inNode);
-	
-
-
-
-
 
 	std::string GetFileName(const char* fileName);
 
-	std::unordered_map<unsigned int, ControlPoint*> mControlPoints;
-	unsigned int mTriangleCount;
-	std::vector<Triangle> mTriangles;
 	std::vector<Vertex> mVertices;
+	//std::vector<Indices> mIndex;
+	std::vector<int>mIndices;
+	unsigned int mPolygonCount;
+
+	HRESULT CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut);
+	BOOL	CompileVertexShader();
+	BOOL	CompilePixelShader();
+
+	BOOL	CreateVertexBuff();
+	BOOL	CreateIndexBuff();
+	BOOL	CreateConstBuff();
+
+	ID3D11VertexShader*     mVertexShader = NULL;
+	ID3D11PixelShader*      mPixelShader = NULL;
+
+	ID3D11InputLayout*      mVertexLayout11 = NULL;
+
+	ID3D11Buffer*           mVertexBuffer = NULL;
+	ID3D11Buffer*           mIndexBuffer = NULL;
+	ID3D11Buffer*           mVSConstBuffer = NULL;
+	ID3D11Buffer*			mPSConstBuffer = NULL;
+
+	D3DXMATRIX				mWorld;
+
+	// get to Renderer
+	ID3D11Device*           mD3DDevice = NULL;
+	ID3D11DeviceContext*    mD3DDeviceContext = NULL;
+
+	// get last error
+	HRESULT hr = S_OK;
 
 };
 
