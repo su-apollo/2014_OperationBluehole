d9a001d6add8f22411dbfdbdbec885bfafc8da48
junggang <missjingi@gmail.com>
2014-10-23 15:03:46 +0900

    LoadTexture 구현중. - 모델에서 텍스쳐 파일만 바꿨기 때문에 경로 직접 넣었음.

--- DeferredShading/DeferredShading/Elin.cpp
+++ DeferredShading/DeferredShading/Elin.cpp
@@ -47,15 +47,15 @@ BOOL Elin::Init()
 	//매쉬 전체로 바꿔야함 위험함.
 
 
-	for (int i = 0; i < mModel.size(); ++i)
+	for (unsigned int i = 0; i < mModel.size(); ++i)
 	{
 		if (!CreateMeshBuffer(mModel[i]))
 			return FALSE;
 	}
 
 	D3DXMATRIX matRotate;
-	D3DXMatrixRotationY(&matRotate, 180.0*(3.14 / 180.0));
-	D3DXMatrixRotationX(&matRotate, -90.0*(3.14 / 180.0));
+	D3DXMatrixRotationY(&matRotate, static_cast<float>(180.0*(3.14 / 180.0)));
+	D3DXMatrixRotationX(&matRotate, static_cast<float>(-90.0*(3.14 / 180.0)));
 
 	mWorld *= matRotate;
 
@@ -144,7 +144,7 @@ void Elin::ProcessGeometry(FbxNode* inNode)
 
 					tempVerts.mPos = currPosition;
 
-					/*
+					
 					float tU;
 					float tV;
 
@@ -153,7 +153,7 @@ void Elin::ProcessGeometry(FbxNode* inNode)
 					tempVerts.mUV.x = tU;
 					tempVerts.mUV.y = tV;
 
-					*/
+					
 					D3DXVECTOR4 currColor;
 					currColor = { 1.0f, 0.0f, 0.0f, 1.0f };
 					tempVerts.Color = currColor;
@@ -305,10 +305,11 @@ void Elin::Render()
 	mD3DDeviceContext->PSSetShader(mPixelShader, NULL, 0);
 	mD3DDeviceContext->PSSetConstantBuffers(0, 1, &mPSConstBuffer);
 
+	//여기서 텍스쳐 그릴거 갱신하는거냐
 	//mD3DDeviceContext->PSSetShaderResources(0, 1, &mTextureRV);
 	//mD3DDeviceContext->PSSetSamplers(0, 1, &mSamplerLinear);
 
-	for (int i = 0; i < mMeshData.size(); ++i)
+	for (unsigned int i = 0; i < mMeshData.size(); ++i)
 	{
 		RenderMesh(mMeshData[i]);
 	}
@@ -512,4 +513,60 @@ BOOL Elin::CreateMeshCB(EMesh mesh, EMeshData meshData)
 
 	return TRUE;
 
+}
+
+BOOL Elin::LoadTexture()
+{
+	mModel[0]->mTexutreDiff = ELIN_TEXTURE_HAND_DIFF;
+	mModel[0]->mTexutreNorm = ELIN_TEXTURE_HAND_NORM;
+	mModel[0]->mTexutreSpec = ELIN_TEXTURE_HAND_SPEC;
+
+	mModel[1]->mTexutreDiff = ELIN_TEXTURE_LEG_DIFF;
+	mModel[1]->mTexutreNorm = ELIN_TEXTURE_LEG_NORM;
+	mModel[1]->mTexutreSpec = ELIN_TEXTURE_LEG_SPEC;
+
+	mModel[2]->mTexutreDiff = ELIN_TEXTURE_BODY_DIFF;
+	mModel[2]->mTexutreNorm = ELIN_TEXTURE_BODY_NORM;
+	mModel[2]->mTexutreSpec = ELIN_TEXTURE_BODY_SPEC;
+
+	mModel[3]->mTexutreDiff = ELIN_TEXTURE_HAIR_DIFF;
+	mModel[3]->mTexutreNorm = ELIN_TEXTURE_HAIR_NORM;
+	mModel[3]->mTexutreSpec = ELIN_TEXTURE_HAIR_SPEC;
+
+	mModel[4]->mTexutreDiff = ELIN_TEXTURE_FACE_DIFF;
+	mModel[4]->mTexutreNorm = ELIN_TEXTURE_FACE_NORM;
+	mModel[4]->mTexutreSpec = ELIN_TEXTURE_FACE_SPEC;
+
+	for (unsigned int i = 0; i < mModel.size(); ++i)
+	{
+		if (!LoadMeshTexture(mModel[i],mMeshData[i]))
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+BOOL Elin::LoadMeshTexture(EMesh mesh, EMeshData meshData)
+{
+	// Load the Texture
+	hr = D3DX11CreateShaderResourceViewFromFile(mD3DDevice, mesh->mTexutreDiff, NULL, NULL, &meshData->mTextureRV, NULL);
+	if (FAILED(hr))
+		return FALSE;
+
+	// Create the sample state
+	D3D11_SAMPLER_DESC sampDesc;
+	ZeroMemory(&sampDesc, sizeof(sampDesc));
+	// 선형 필터
+	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	sampDesc.MinLOD = 0;
+	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	hr = mD3DDevice->CreateSamplerState(&sampDesc, &meshData->mSamplerLinear);
+	if (FAILED(hr))
+		return FALSE;
+
+	return TRUE;
 }
--- DeferredShading/DeferredShading/Elin.h
+++ DeferredShading/DeferredShading/Elin.h
@@ -4,13 +4,35 @@
 #include <xnamath.h>
 #include "Singleton.h"
 
+static const LPCWSTR	ELIN_TEXTURE_FACE_DIFF = L"ElinModel/Popori_F_Face01_diff.bmp";
+static const LPCWSTR	ELIN_TEXTURE_FACE_NORM = L"ElinModel/Popori_F_Face01_norm.bmp";
+static const LPCWSTR	ELIN_TEXTURE_FACE_SPEC = L"ElinModel/Popori_F_Face01_spec.bmp";
+
+static const LPCWSTR	ELIN_TEXTURE_BODY_DIFF = L"ElinModel/Popori_F_H00_Body_diff.bmp";
+static const LPCWSTR	ELIN_TEXTURE_BODY_NORM = L"ElinModel/Popori_F_H00_Body_norm.bmp";
+static const LPCWSTR	ELIN_TEXTURE_BODY_SPEC = L"ElinModel/Popori_F_H00_Body_spec.bmp";
+
+static const LPCWSTR	ELIN_TEXTURE_HAND_DIFF = L"ElinModel/Popori_F_H00_Hand_diff.bmp";
+static const LPCWSTR	ELIN_TEXTURE_HAND_NORM = L"ElinModel/Popori_F_H00_Hand_norm.bmp";
+static const LPCWSTR	ELIN_TEXTURE_HAND_SPEC = L"ElinModel/Popori_F_H00_Hand_spec.bmp";
+
+static const LPCWSTR	ELIN_TEXTURE_LEG_DIFF = L"ElinModel/Popori_F_H00_Leg_diff.bmp";
+static const LPCWSTR	ELIN_TEXTURE_LEG_NORM = L"ElinModel/Popori_F_H00_Leg_norm.bmp";
+static const LPCWSTR	ELIN_TEXTURE_LEG_SPEC = L"ElinModel/Popori_F_H00_Leg_spec.bmp";
+
+static const LPCWSTR	ELIN_TEXTURE_HAIR_DIFF = L"ElinModel/popori_F_hair07_diff.bmp";
+static const LPCWSTR	ELIN_TEXTURE_HAIR_NORM = L"ElinModel/popori_F_hair07_norm.bmp";
+static const LPCWSTR	ELIN_TEXTURE_HAIR_SPEC = L"ElinModel/popori_F_hair07_spec.bmp";
+
+
+
 struct Vertex
 {
 	D3DXVECTOR3 mPos;
 	D3DXVECTOR4 Color;
+	D3DXVECTOR2 mUV;
 
 	//D3DXVECTOR3 mNormal;
-	//D3DXVECTOR2 mUV;
 
 };
 
@@ -22,6 +44,9 @@ struct Mesh
 	int mNumVertex;
 	int mNumIndex;
 
+	const WCHAR* mTexutreDiff;
+	const WCHAR* mTexutreNorm;
+	const WCHAR* mTexutreSpec;
 };
 
 struct Indices
@@ -35,6 +60,9 @@ struct MeshData
 	ID3D11Buffer*           mIndexBuffer = NULL;
 
 	int						mNumIndex = 0;
+
+	ID3D11ShaderResourceView*	mTextureRV = NULL;
+	ID3D11SamplerState*			mSamplerLinear = NULL;
 };
 
 typedef std::shared_ptr<Mesh> EMesh;
@@ -68,6 +96,7 @@ private:
 	BOOL	CreateMeshCB(EMesh mesh, EMeshData meshData);
 
 	BOOL LoadTexture();
+	BOOL LoadMeshTexture(EMesh mesh, EMeshData meshData);
 
 	std::string GetFileName(const char* fileName);
 
