a85fe6c9e006aad7df53ef0942bd7bfbfbbe0d19
양현찬 <e7315126@naver.com>
2014-10-31 18:11:46 +0900

    commit resource

--- DeferredShading/DeferredShading/Contents.cpp
+++ DeferredShading/DeferredShading/Contents.cpp
@@ -19,8 +19,8 @@ void Contents::Init()
 	LightManager::GetInstance()->CreatePointLights(MAX_LIGHT);
 
 	//todo : ºû À§Ä¡ ¼³Á¤
-	LightManager::GetInstance()->mPLightList[1]->mPos = D3DXVECTOR4(-0.1f, 0.f, -0.1f, 1.0f);
-	LightManager::GetInstance()->mPLightList[1]->mColor = D3DXVECTOR4(1, 0, 0, 1);
+	LightManager::GetInstance()->mPLightList[1]->mPos = D3DXVECTOR4(-50.f, 0.f, -50.f, 1.0f);
+	LightManager::GetInstance()->mPLightList[1]->mColor = D3DXVECTOR4(1, 1, 1, 1);
 }
 
 void Contents::Render()
