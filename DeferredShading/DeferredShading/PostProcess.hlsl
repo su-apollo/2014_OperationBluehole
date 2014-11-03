7e14a199560e7e32d22366bcb8b9b3ba6f597d73
junggang <missjingi@gmail.com>
2014-11-03 10:48:06 +0900

    SSAO 구현중. 일단은 sphere.

--- DeferredShading/DeferredShading/PostProcess.hlsl
+++ DeferredShading/DeferredShading/PostProcess.hlsl
@@ -36,38 +36,40 @@ struct PS_INPUT
 	float2 Tex : TEXCOORD0;
 };
 
+
+
+
 //--------------------------------------------------------------------------------------
 // SSAO
 //--------------------------------------------------------------------------------------
-/*
-float getOcclusion(float3x3 tbn, float4 position)
+float getOcclusion(float3x3 tbn, float4 viewPosition)
 {
 	float3 sample_sphere[8] = {
-		float3(0.5381, -0.4856, 0.4319), float3(0.1379, 0.2486, 0.4430),
-		float3(0.3371, 0.5679, 0.0057), float3(-0.6999, -0.2451, -0.0019),
-		float3(0.4689, -0.1598, 0.8547), float3(0.2560, 0.8069, 0.1843),
-		float3(-0.4146, 0.1402, 0.0762), float3(-0.7100, -0.1924, 0.7344)
+		float3(0.7, 0.7, 0), float3(0, 0.7, 0.7),
+		float3(-0.7, 0.7, 0), float3(0, 0.7, -0.7),
+		float3(0.7, -0.7, 0), float3(0, -0.7, 0.7),
+		float3(-0.7, -0.7, 0), float3(0, -0.7, -0.7)
 	};
 
-	float radius = 0.5f;
+	float radius = 2;
 
 	float occlusion = 0.0f;
-
 	for (int i = 0; i < 8; ++i)
 	{
 		//make sample kernels
-		float3 sampleViewPosition = sample_sphere[i]; 
+		float3 sampleViewPosition = sample_sphere[i];
 		sampleViewPosition = mul(tbn, sampleViewPosition);
-		sampleViewPosition = sampleViewPosition * radius + position.xyz;
+		sampleViewPosition = sampleViewPosition * radius + viewPosition.xyz;
 
 		//project position
-		float4 sampleProjected = mul(mProj, float4(sampleViewPosition, 1)); // -1~1
+		float4 sampleProjected = mul(float4(sampleViewPosition, 1), mProj); // -1~1
 		sampleProjected.xy /= sampleProjected.w;
 		float2 projected = float2(sampleProjected.x, sampleProjected.y);
-		sampleProjected.xy = sampleProjected.xy*0.5 + 0.5; 
+		float2 a = float2(sampleProjected.x*0.5 + 0.5, 0.5 - sampleProjected.y*0.5);
+		//sampleProjected.xy = sampleProjected.xy*0.5 + 0.5; 
 
 		//get original depth
-		float sampleOriginalDepth = txDepth.Sample(samLinear, sampleProjected.xy).x;
+		float	sampleOriginalDepth = txDepth.Sample(samLinear, a).x;
 		float4 originalViewPos = mul(float4(projected.x, projected.y, sampleOriginalDepth, 1), mInverseProj);
 		originalViewPos /= originalViewPos.w;
 
@@ -76,10 +78,28 @@ float getOcclusion(float3x3 tbn, float4 position)
 	}
 
 	//more occluded means darker.
-	occlusion = 1 - (occlusion / 8);
+	occlusion = 1 - (occlusion / 8) * 0.5;
+
 	return occlusion;
 }
-*/
+
+
+float3 normal_from_depth(float depth, float2 texcoords) {
+
+	const float2 offset1 = float2(0.0, 0.0001);
+	const float2 offset2 = float2(0.0001, 0.0);
+
+	float depth1 = txDepth.Sample(samLinear, texcoords + offset1).x;
+	float depth2 = txDepth.Sample(samLinear, texcoords + offset2).x;
+
+	float3 p1 = float3(offset1, depth1 - depth);
+	float3 p2 = float3(offset2, depth2 - depth);
+
+	float3 normal = cross(p1, p2);
+	normal.z = -normal.z;
+
+	return normalize(normal);
+}
 //--------------------------------------------------------------------------------------
 // Pixel Shader
 //--------------------------------------------------------------------------------------
@@ -93,8 +113,8 @@ float4 main(PS_INPUT Input) : SV_TARGET
 	float4 ambient = float4(0, 0, 0, 1)*0.3;
 
 	normal = normal * 2 - 1;
-	
-	// reconstruct pos
+
+	// reconstruct world pos
 	float4 position;
 	position.x = Input.Tex.x * 2 - 1;
 	position.y = (1 - Input.Tex.y) * 2 - 1;
@@ -102,11 +122,11 @@ float4 main(PS_INPUT Input) : SV_TARGET
 	position.w = 1;
 	position = mul(position, mInverseViewProj);
 	position /= position.w;
-	
+
 	float4 diffuseFactor = float4(0, 0, 0, 0);
 	float4 specularFactor = float4(0, 0, 0, 0);;
-	
-	for ( int i = 0; i < 1 ; ++i )
+
+	for (int i = 0; i < 1; ++i)
 	{
 		// get lightDirection and distance
 		float4 lightDir = vLightPos[i] - position;
@@ -114,7 +134,7 @@ float4 main(PS_INPUT Input) : SV_TARGET
 		lightDir /= distance;
 
 		float3 attr = float3(0, 0, 1);
-		float attrFactor = 1.0f - saturate((distance-0.1f) / vLightRange[i].x);
+		float attrFactor = 1.0f - saturate((distance - 0.1f) / vLightRange[i].x);
 		attrFactor = pow(attrFactor, 2);
 
 		//calculate diffuseFactor
@@ -138,7 +158,7 @@ float4 main(PS_INPUT Input) : SV_TARGET
 	float2 noiseTexCoords = float2(1024.0f, 768.0f) / float2(128.0f, 128);
 	noiseTexCoords *= Input.Tex;
 	float4 noise = txNoise.Sample(samLinear, noiseTexCoords);
-	
+
 	//get view position
 	float4 viewPosition;
 	viewPosition.x = Input.Tex.x * 2 - 1;
@@ -148,23 +168,29 @@ float4 main(PS_INPUT Input) : SV_TARGET
 	viewPosition = mul(viewPosition, mInverseProj);
 	viewPosition /= viewPosition.w;
 
-
+	//get view Normal ???
 	/*
-	//make randomVector -1 ~ 1
-	float3 randomVector = noise.xyz *  2.0 - 1.0;
-	float3 tangent = normalize(randomVector - normal.xyz*dot(randomVector, normal.xyz));
-	float3 bitangent = cross(tangent, normal.xyz);
-	float3x3 kernelTBN = float3x3(tangent, bitangent, normal.xyz);
-
-
-	float occlusionFactor = getOcclusion(kernelTBN, viewPosition);
+	float4 viewNormal = txNormal.Sample(samLinear, Input.Tex);
+	viewNormal = viewNormal * 2 - 1;
 	*/
 
+	float3 viewNormal = normal_from_depth(depth.x, Input.Tex);
+	//viewNormal += normal;
+
+	//make randomVector -1 ~ 1
+	float3 randomVector = noise.xyz *  2.0 - 1.0;
+	float3 tangent = normalize(randomVector - viewNormal.xyz*dot(randomVector, viewNormal.xyz));
+	float3 bitangent = cross(tangent, viewNormal.xyz);
+	float3x3 kernelTBN = float3x3(tangent, bitangent, viewNormal.xyz);
+
+	float occlusion = getOcclusion(kernelTBN, viewPosition);
+
 	float4 finalColor = 0;
-	finalColor = saturate(ambient + specular*4 + diffuse);
+	finalColor = float4(occlusion, occlusion, occlusion, 1);
+	//finalColor = saturate(ambient + specular*4 + diffuse);
 	//finalColor = specular * 4;
-	//finalColor = float4(viewPosition.xxx, 1);
-	//finalColor = float4(depth.xxx,1);
+	//finalColor = float4(sampleOriginalDepth, sampleOriginalDepth, sampleOriginalDepth, 1);
+	//finalColor = float4(originalViewPos.yyy, 1);
 	//finalColor = float4(z, z, z, 1);
 	return finalColor;
 }
