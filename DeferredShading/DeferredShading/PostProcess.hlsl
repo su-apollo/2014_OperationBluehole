46805ea0c6476e29fd61f7521cd932a1f9b4bef3
junggang <missjingi@gmail.com>
2014-11-06 10:57:47 +0900

    차폐 계산 후에 테두리가 진해졌던 현상 해결.

--- DeferredShading/DeferredShading/PostProcess.hlsl
+++ DeferredShading/DeferredShading/PostProcess.hlsl
@@ -43,7 +43,7 @@ struct PS_INPUT
 //--------------------------------------------------------------------------------------
 // SSAO
 //--------------------------------------------------------------------------------------
-float getOcclusion(float3x3 tbn, float4 position)
+float getOcclusion(float3x3 tbn, float4 position, float4 normal)
 {
 	//set z values to 0~1 cuz we want to orient kernels on z axis.
 	float3 sample_sphere[8] = {
@@ -73,19 +73,23 @@ float getOcclusion(float3x3 tbn, float4 position)
 		float2 projCoord = float2(sampleProjected.x*0.5 + 0.5, 0.5 - sampleProjected.y*0.5); // 0~1
 
 		//get original depth
-		float	origianlDepth = txDepth.Sample(samLinear, projCoord).x; // 0~1
-		float4 origianlWorldPos = mul(float4(sampleProjected.x, sampleProjected.y, origianlDepth, 1), mInverseViewProj);
-		origianlWorldPos /= origianlWorldPos.w;
+		float	originalDepth = txDepth.Sample(samLinear, projCoord).x; // 0~1
+		float4 originalWorldPos = mul(float4(sampleProjected.x, sampleProjected.y, originalDepth, 1), mInverseViewProj);
+		originalWorldPos /= originalWorldPos.w;
 
 		//optional calculation? sampleViewPosition.z > originalPos.z면 차폐된것이다.
-		float rangeCheck = (distance(position.xyz, sampleWorldPos.xyz) < radius) ? 1 : 0;
-		//occlusion += step(origianlWorldPos.z, sampleWorldPos.z)* rangeCheck;
-		occlusion += step(origianlDepth, sampleProjected.z)* rangeCheck;
+		float rangeCheck = max(dot(normal.xyz, normalize(originalWorldPos.xyz - position.xyz)), 0);
+
+		//거리에 따라 차폐에 기여하는 정도를 계산
+		float dist = sampleProjected.z - originalDepth;
+		occlusion += saturate((radius*0.8 - dist) / radius) * rangeCheck;
+
+		//occlusion += step(originalDepth, sampleProjected.z)* rangeCheck;
 
 	}
 
 	//more occluded means darker.
-	occlusion = 1 - (occlusion / 8) * 0.8;
+	occlusion = 1 - (occlusion / 8);
 
 	return occlusion;
 }
@@ -145,6 +149,7 @@ float4 main(PS_INPUT Input) : SV_TARGET
 		lightDir /= distance;
 
 		float3 attr = float3(0, 0, 1);
+		//빛 풀파워 주는 영역 변수로 고칠 것.
 		float attrFactor = 1.0f - saturate((distance - 0.1f) / vLightRange[i].x);
 		attrFactor = pow(attrFactor, 2);
 
@@ -165,8 +170,7 @@ float4 main(PS_INPUT Input) : SV_TARGET
 
 
 	//SSAO
-	//texture size 어떻게 얻지? 일단 숫자로 넣어놨지만 찾아서 바꿀 것.
-	float2 noiseTexCoords = float2(1024.0f, 768.0f) / float2(64, 64);
+	float2 noiseTexCoords = float2(1024.0f, 768.0f) / float2(128, 128);
 	noiseTexCoords *= Input.Tex;
 	float4 noise = txNoise.Sample(samLinear, noiseTexCoords);
 	//set noise.z to 0 cuz we want to orient hemisphere on z axis.
@@ -180,10 +184,10 @@ float4 main(PS_INPUT Input) : SV_TARGET
 	float3 bitangent = cross(tangent, normal.xyz);
 	float3x3 kernelTBN = float3x3(tangent, bitangent, normal.xyz);
 
-	float occlusion = getOcclusion(kernelTBN, position);
+	float occlusion = getOcclusion(kernelTBN, position,normal);
 	ambient *= occlusion;
 
-	float4 finalColor = 0;
+	float4 finalColor = normal;
 	//finalColor = saturate(ambient+diffuse+specular);
 	//finalColor = ambient;
 	finalColor = float4(occlusion, occlusion, occlusion, 1);
