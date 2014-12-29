d5b5941c0d9fa80a33a30f2eb9cf930ebf0e9bef
양현찬 <e7315126@naver.com>
2014-12-29 16:47:18 +0900

    빌보드용 셰이더파일 추가

--- DeferredShading/DeferredShading/Billboard.cpp
+++ DeferredShading/DeferredShading/Billboard.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "Billboard.h"
-
+#include "Renderer.h"
 
 
 
@@ -12,3 +12,122 @@ Billboard::Billboard()
 Billboard::~Billboard()
 {
 }
+
+BOOL Billboard::Init()
+{
+	mD3DDevice = Renderer::GetInstance()->GetDevice();
+	mD3DDeviceContext = Renderer::GetInstance()->GetDeviceContext();
+
+	return TRUE;
+}
+
+BOOL Billboard::CreateQuad()
+{
+	QuadVertex verts[4] =
+	{
+		{ D3DXVECTOR3(1, 1, 0), D3DXVECTOR2(1, 0) },
+		{ D3DXVECTOR3(1, -1, 0), D3DXVECTOR2(1, 1) },
+		{ D3DXVECTOR3(-1, -1, 0), D3DXVECTOR2(0, 1) },
+		{ D3DXVECTOR3(-1, 1, 0), D3DXVECTOR2(0, 0) }
+	};
+
+	D3D11_BUFFER_DESC desc;
+	// immutable
+	desc.Usage = D3D11_USAGE_IMMUTABLE;
+	desc.ByteWidth = sizeof(verts);
+	desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	desc.CPUAccessFlags = 0;
+	desc.MiscFlags = 0;
+	D3D11_SUBRESOURCE_DATA initData;
+	initData.pSysMem = verts;
+	initData.SysMemPitch = 0;
+	initData.SysMemSlicePitch = 0;
+	mD3DDevice->CreateBuffer(&desc, &initData, &mVertexBuffer);
+	if (FAILED(hr))
+		return FALSE;
+
+	unsigned short indices[6] = { 0, 1, 2, 2, 3, 0 };
+
+	desc.Usage = D3D11_USAGE_IMMUTABLE;
+	desc.ByteWidth = sizeof(indices);
+	desc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	desc.CPUAccessFlags = 0;
+	initData.pSysMem = indices;
+	mD3DDevice->CreateBuffer(&desc, &initData, &mIndexBuffer);
+	if (FAILED(hr))
+		return FALSE;
+
+	return TRUE;
+}
+
+BOOL Billboard::CompileShader()
+{
+	// vertex shader
+	ID3DBlob* pVSBlob = NULL;
+	hr = CompileShaderFromFile(mVertexShaderPath, mVertexShaderMain, mVertexShaderModel, &pVSBlob);
+	if (FAILED(hr))
+		return FALSE;
+
+	hr = mD3DDevice->CreateVertexShader(pVSBlob->GetBufferPointer(),
+		pVSBlob->GetBufferSize(), NULL, &mVertexShader);
+
+	if (FAILED(hr))
+	{
+		SafeRelease(pVSBlob);
+		return FALSE;
+	}
+
+	// input layout
+	const D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+	UINT numElements = ARRAYSIZE(layout);
+
+	hr = mD3DDevice->CreateInputLayout(layout, numElements, pVSBlob->GetBufferPointer(),
+		pVSBlob->GetBufferSize(), &mVertexLayout11);
+
+	SafeRelease(pVSBlob);
+	if (FAILED(hr))
+		return FALSE;
+
+	// pixel shader
+	ID3DBlob* pPSBlob = NULL;
+	hr = CompileShaderFromFile(mPixelShaderPath, mPixelShaderMain, mPixelShaderModel, &pPSBlob);
+	if (FAILED(hr))
+		return FALSE;
+
+	hr = mD3DDevice->CreatePixelShader(pPSBlob->GetBufferPointer(),
+		pPSBlob->GetBufferSize(), NULL, &mPixelShader);
+
+	SafeRelease(pPSBlob);
+	if (FAILED(hr))
+		return FALSE;
+
+	return TRUE;
+}
+
+HRESULT Billboard::CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut)
+{
+	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS | D3DCOMPILE_PACK_MATRIX_COLUMN_MAJOR;
+
+#if defined( DEBUG ) || defined( _DEBUG )
+	dwShaderFlags |= D3DCOMPILE_DEBUG;
+#endif
+
+	ID3DBlob* pErrorBlob;
+	hr = D3DX11CompileFromFile(szFileName, NULL, NULL, szEntryPoint, szShaderModel,
+		dwShaderFlags, 0, NULL, ppBlobOut, &pErrorBlob, NULL);
+	if (FAILED(hr))
+	{
+		if (pErrorBlob != NULL)
+			OutputDebugStringA((char*)pErrorBlob->GetBufferPointer());
+		if (pErrorBlob) pErrorBlob->Release();
+		return hr;
+	}
+	if (pErrorBlob) pErrorBlob->Release();
+
+	return S_OK;
+}
+
--- DeferredShading/DeferredShading/Billboard.h
+++ DeferredShading/DeferredShading/Billboard.h
@@ -3,7 +3,6 @@
 
 
 
-
 class Billboard
 {
 public:
@@ -11,17 +10,36 @@ public:
 	virtual ~Billboard();
 
 
+	BOOL	Init();
 
+	BOOL	CreateQuad();
+	BOOL	CompileShader();
 
-
+	HRESULT	CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut);
 
 private:
+
+	ID3D11VertexShader*     mVertexShader = NULL;
+	ID3D11PixelShader*		mPixelShader = NULL;
 
 	ID3D11Buffer*           mVertexBuffer = NULL;
 	ID3D11Buffer*           mIndexBuffer = NULL;
 
+	ID3D11InputLayout*      mVertexLayout11 = NULL;
 
+	WCHAR*					mVertexShaderPath = L".hlsl";
+	LPCSTR					mVertexShaderMain = "main";
+	LPCSTR					mVertexShaderModel = "vs_5_0";
 
+	WCHAR*					mPixelShaderPath = L".hlsl";
+	LPCSTR					mPixelShaderMain = "main";
+	LPCSTR					mPixelShaderModel = "ps_5_0";
 
+	// get from renderer
+	ID3D11Device*           mD3DDevice = NULL;
+	ID3D11DeviceContext*    mD3DDeviceContext = NULL;
+
+	// get last error
+	HRESULT hr = S_OK;
 };
 
