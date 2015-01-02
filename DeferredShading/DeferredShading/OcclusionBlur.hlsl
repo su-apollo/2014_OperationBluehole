7650a05e8130bc68b1826a8c86a35246c42388d5
junggang <missjingi@gmail.com>
2015-01-02 17:38:40 +0900

    일단 SSDO radius쪼갬. 이건 랜덤하지 않아도 될 듯함.

--- DeferredShading/DeferredShading/OcclusionBlur.hlsl
+++ DeferredShading/DeferredShading/OcclusionBlur.hlsl
@@ -26,9 +26,9 @@ SamplerState samLinear : register(s0);
 float4 SsdoBlur(float blurSize, float texelSize, PS_INPUT Input)
 {
 	float3 blurredBounce = float3(0, 0, 0);
-	float  blurredOcclusion = 0.0f;
+		float  blurredOcclusion = 0.0f;
 
-	float weight[25] = { 
+	float weight[25] = {
 		0.031827f, 0.037541f, 0.039665f, 0.037541f, 0.031827f,
 		0.037541f, 0.044281f, 0.046787f, 0.044281f, 0.037541f,
 		0.039665f, 0.046787f, 0.049434f, 0.046787f, 0.039665f,
@@ -38,14 +38,14 @@ float4 SsdoBlur(float blurSize, float texelSize, PS_INPUT Input)
 
 	float2 hlim = (float(-blurSize) * 0.5f);
 
-	[unroll]
-	for (int i = 0; i < blurSize; ++i) 
+		[unroll]
+	for (int i = 0; i < blurSize; ++i)
 	{
-		for (int j = 0; j < blurSize; ++j) 
+		for (int j = 0; j < blurSize; ++j)
 		{
 			float2 offset = (hlim + float2(float(i), float(j))) * texelSize;
-			blurredBounce += (txSSDO.Sample(samLinear, Input.Tex + offset*2).xyz*weight[blurSize*i + j]);
-			blurredOcclusion += (txSSDO.Sample(samLinear, Input.Tex + offset*2).a*weight[blurSize*i + j]);
+				blurredBounce += (txSSDO.Sample(samLinear, Input.Tex + offset * 2).xyz*weight[blurSize*i + j]);
+			blurredOcclusion += (txSSDO.Sample(samLinear, Input.Tex + offset * 2).a*weight[blurSize*i + j]);
 		}
 	}
 
@@ -61,20 +61,21 @@ float4 main(PS_INPUT Input) : SV_TARGET
 	float2 texSize;
 	txSSDO.GetDimensions(texSize.x, texSize.y);
 	float2 texelSize = 1.0 / texSize;
-	float blurSize = 5;
+		float blurSize = 5;
 
 	float4 blurredSSDO = SsdoBlur(blurSize, texelSize, Input);
 
-	//ambient *= blurredSSDO.a;
-	//ambient *= 0.2;
-	//diffSpec += blurredSSDO.xyz*0.5;
+		//ambient *= blurredSSDO.a;
+		//ambient *= 0.2;
 
-	
-	
-	return float4(diffSpec,1);
-	//return float4(saturate(ssdo.aaa), 1);
-	
-	//return float4(saturate(blurredSSDO.aaa), 1);
+		//diffSpec += blurredSSDO.xyz*0.5;
+
+
+
+	return float4(diffSpec, 1);
+	//return float4(saturate(ssdo.xyz), 1);
+
+	//return float4(saturate(blurredSSDO.xyz), 1);
 	//return float4(saturate(diffSpec*blurredSSDO.a + ambient*0.15*blurredSSDO.a), 1);
 	//return float4(saturate(blurredSSDO.xyz), 1);
 	//return float4(saturate(diffSpec + blurredSSDO.xyz*0.3 + ambient*0.1*blurredSSDO.a), 1);
--- DeferredShading/DeferredShading/PostProcess.hlsl
+++ DeferredShading/DeferredShading/PostProcess.hlsl
@@ -52,8 +52,8 @@ struct OUT
 float getOcclusion(float3x3 tbn, float4 position, float4 normal)
 {
 	float radius = vKernelVariables.x;
-
 	float occlusion = 0.0f;
+	float bounceRange = 50.0f;
 
 	[unroll]
 	for (int i = 0; i < 8; ++i)
@@ -84,7 +84,18 @@ float getOcclusion(float3x3 tbn, float4 position, float4 normal)
 
 		// check if it's an occluder
 		float dist = sampleProjected.z - originalDepth;
+		//get a vector from original pos to position pos
+		float4 originalToPos = position - originalWorldPos;
+		//get a distance
+		float distance = length(originalToPos);
 		if (dist < 0) rangeCheck = 0;
+
+		if (sampleProjected.x<-1)rangeCheck = 0;
+		if (sampleProjected.y<-1)rangeCheck = 0;
+		if (sampleProjected.x>1)rangeCheck = 0;
+		if (sampleProjected.y>1)rangeCheck = 0;
+
+		if (distance > bounceRange) rangeCheck = 0;
 
 		// occlusion value decreases with distance.
 		occlusion += saturate((radius*0.8 - dist) / radius) * rangeCheck;
@@ -96,7 +107,69 @@ float getOcclusion(float3x3 tbn, float4 position, float4 normal)
 	return occlusion;
 }
 
+float3 ComputeSSDO(float3x3 tbn, float4 position, float4 normal)
+{
+	float3 bouncedColor = float3(0, 0, 0);
+	float radius = 20.0f;
+	float bounceRange = 50.0f;
 
+	[unroll]
+	for (int i = 0; i < 8; ++i)
+	{
+		//make sample kernels
+		float3 sampleWorldPos = vSampleSphere[i].xyz;
+		sampleWorldPos = mul(tbn, sampleWorldPos);
+
+		float scale = float(i) / float(8);
+		scale = lerp(0.3f, 1.0f, scale * scale);
+		sampleWorldPos *= scale;
+
+		sampleWorldPos = -sampleWorldPos * radius + position.xyz;
+
+		//project position
+		float4 sampleProjected = mul(float4(sampleWorldPos, 1), mViewProj); // -1~1
+		sampleProjected.xyz /= sampleProjected.w;
+		float2 projCoord = float2(sampleProjected.x*0.5 + 0.5, 0.5 - sampleProjected.y*0.5); // 0~1
+
+		//get original depth
+		float	originalDepth = txDepth.Sample(samLinear, projCoord).x; // 0~1
+		float4 originalWorldPos = mul(float4(sampleProjected.x, sampleProjected.y, originalDepth, 1), mInverseViewProj);
+		originalWorldPos /= originalWorldPos.w;
+
+		//get original normal
+		float4 originalWorldNormal = txNormal.Sample(samLinear, projCoord);
+
+		//get original diffuse color
+		float4 originalColor = txDiffuse.Sample(samLinear, projCoord);
+		//get a vector from original pos to position pos
+		float4 originalToPos = position - originalWorldPos;
+		//get a distance
+		float distance = length(originalToPos);
+		//normalize vector
+		originalToPos = normalize(originalToPos);
+
+		// the sample point too close to original point must be ignored.
+		// scale해주는게 나을듯.
+		float rangeCheck = max(dot(normal.xyz, normalize(originalWorldPos.xyz - position.xyz)), 0);
+		if (originalDepth == 1) rangeCheck = 0;
+
+		// check if it's an occluder
+		float dist = sampleProjected.z - originalDepth;
+		if (dist < 0) rangeCheck = 0;
+
+		if (sampleProjected.x<-1)rangeCheck = 0;
+		if (sampleProjected.y<-1)rangeCheck = 0;
+		if (sampleProjected.x>1)rangeCheck = 0;
+		if (sampleProjected.y>1)rangeCheck = 0;
+
+		if (distance > bounceRange) rangeCheck = 0;
+
+		bouncedColor += originalColor.xyz*max(dot(originalWorldNormal, originalToPos), 0)*max(((radius - distance) / radius), 0) *rangeCheck;
+	}
+	return saturate(bouncedColor);
+}
+
+/*
 //--------------------------------------------------------------------------------------
 // SSDO : occlusion + one indirect bounce
 // return float4(indirect color.xyz,occlusion)
@@ -172,6 +245,7 @@ float4 ComputeSSDO(float3x3 tbn, float4 position, float4 normal)
 	return saturate(float4(bouncedColor,occlusion));
 }
 
+*/
 //--------------------------------------------------------------------------------------
 // Pixel Shader
 //--------------------------------------------------------------------------------------
@@ -225,7 +299,7 @@ OUT main(PS_INPUT Input) : SV_TARGET
 
 			//calculate specularFactor
 			float vDotN = dot(viewDir, reflection);
-			specularFactor += pow(vDotN, 2.0f) * vLightColor[i] * attrFactor;
+			specularFactor += pow(vDotN, 1.0f) * vLightColor[i] * attrFactor;
 		}
 		
 	}
@@ -250,12 +324,12 @@ OUT main(PS_INPUT Input) : SV_TARGET
 	float3x3 kernelTBN = float3x3(tangent, bitangent, normal.xyz);
 
 	// only SSAO
-	//float occlusion = getOcclusion(kernelTBN, position,normal);
+	float occlusion = getOcclusion(kernelTBN, position,normal);
 
-	float4 ssdo = ComputeSSDO(kernelTBN, position, normal);
+	float3 ssdo = ComputeSSDO(kernelTBN, position, normal);
 
 	OUT output;
-	output.SSDO = ssdo;
+	output.SSDO = float4(ssdo, occlusion);
 	output.DiffSpec = float4(saturate((0.8*diffuse+0.5*specular).xyz), 1);
 
 	return output;
